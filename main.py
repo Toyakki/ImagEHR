1c6a57b8259097c06809b6849c698678c6fe5a94
Evan Su <48808396+HACKERALERT@users.noreply.github.com>
2025-03-22 11:08:22 -0400

    uncomment cohere imports

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template
 from flask_cors import CORS
-
-# from genai.cohere_helper import simple_chat
+from genai.cohere_helper import simple_chat
 
 app = Flask(__name__)
 CORS(app)
@@ -9,13 +8,13 @@ CORS(app)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+	return render_template("index.html")
 
 
 @app.route("/getstarted")
 def upload():
-    return render_template("upload.html")
+	return render_template("upload.html")
 
 
 if __name__ == "__main__":
-    app.run(port=8000, host="0.0.0.0")  # NOT FOR PRODUCTION
+	app.run(port=8000, host="0.0.0.0")  # NOT FOR PRODUCTION
