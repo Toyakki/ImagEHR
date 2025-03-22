39a5b798ab131135cc52ac74e747142660b22fe3
Evan Su <48808396+HACKERALERT@users.noreply.github.com>
2025-03-21 23:23:21 -0400

    add example api_base url

--- fhir/__init__.py
+++ fhir/__init__.py
@@ -1,6 +1,9 @@
 from fhirclient import client
 
 def get_client(api_base: str) -> client.FHIRClient:
+	"""
+	>>> get_client("https://hapi.fhir.org/baseR4")
+	"""
 	settings = {
 		"app_id": "imagehr_bg_worker",
 		"api_base": api_base
