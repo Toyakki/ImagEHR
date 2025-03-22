6263d09cb2b375c0b9b144fd4aa22cdcf66b1a1f
Evan Su <48808396+HACKERALERT@users.noreply.github.com>
2025-03-21 23:30:45 -0400

    Update __init__.py

--- fhir/__init__.py
+++ fhir/__init__.py
@@ -1,6 +1,7 @@
-from fhirclient import client
+from fhirclient.client import FHIRClient
+from fhirclient.models.patient import Patient
 
-def get_client(api_base: str) -> client.FHIRClient:
+def get_client(api_base: str) -> FHIRClient:
 	"""
 	>>> get_client("https://hapi.fhir.org/baseR4")
 	"""
@@ -8,4 +9,10 @@ def get_client(api_base: str) -> client.FHIRClient:
 		"app_id": "imagehr_bg_worker",
 		"api_base": api_base
 	}
-	return client.FHIRClient(settings=settings)
+	return FHIRClient(settings=settings)
+
+def get_all_patient_ids(cli: FHIRClient) -> set[str]:
+	search = Patient.where(struct={})
+	patients = search.perform_resources(cli.server)
+	patient_ids = [patient.id for patient in patients]
+	return set(patient_ids)
