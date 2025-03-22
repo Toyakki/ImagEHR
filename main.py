711f637c835f42fcb8ec065cbd31078d8b5d9ce2
Shaaf Yousaf <muhdshaaf@gmail.com>
2025-03-22 09:30:24 -0400

    Fixed routing issue on Website

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 from flask import Flask, render_template
 from flask_cors import CORS
+
 # from genai.cohere_helper import simple_chat
 
 app = Flask(__name__)
 CORS(app)
 
+
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route("/getstarted")
+def upload():
+    return render_template("upload.html")
 
 
 if __name__ == "__main__":
