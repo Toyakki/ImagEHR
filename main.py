277cbb12ab0dc15805317ed45b1ae63c2dae0229
Evan Su <48808396+HACKERALERT@users.noreply.github.com>
2025-03-22 15:54:35 -0400

    set a global fhir_api_base on post form

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template, request, redirect
 from flask_cors import CORS
+from fhir import get_all_patient_ids
 from genai.cohere_helper import simple_chat # DO NOT REMOVE OR COMMENT OUT
 from model.inference import inference # DO NOT REMOVE OR COMMENT OUT
 
 app = Flask(__name__)
 CORS(app)
+fhir_api_base: str = ""
 
 @app.route("/")
 def index():
@@ -16,9 +18,19 @@ def upload():
 
 @app.route("/set-fhir", methods=["GET", "POST"])
 def set_fhir():
+	global fhir_api_base
 	if request.method == "GET":
 		return render_template("set-fhir.html")
-	print(request.form)
+	if "fhir" not in request.form:
+		return "<h1>Please enter a value for FHIR API endpoint.</h1>", 400
+	fhir_ = request.form["fhir"]
+	try:
+		patient_ids = get_all_patient_ids(fhir_)
+		if not patient_ids:
+			raise Exception
+	except:
+		return "<h1>Failed to get patient IDs from FHIR. Invalid URL?</h1>", 400
+	fhir_api_base = fhir_
 	return redirect("/dashboard")
 
 if __name__ == "__main__":
