ba5322d4df9c8b563e0943f6413eca826452adef
Evan Su <48808396+HACKERALERT@users.noreply.github.com>
2025-03-22 16:18:15 -0400

    create fhir.get_patient() -> Patient

--- fhir/__init__.py
+++ fhir/__init__.py
@@ -16,6 +16,15 @@ def get_all_patient_ids(api_base: str) -> list[str]:
 	patient_ids = [patient.id for patient in patients]
 	return patient_ids
 
+def get_patient(api_base: str, patient_id: str) -> Patient:
+	settings = {
+		"app_id": "imagehr_bg_worker",
+		"api_base": api_base
+	}
+	client = FHIRClient(settings=settings)
+	patient = Patient.read(patient_id, client.server)
+	return patient
+
 def get_all_patient_info(api_base: str, patient_id: str) -> str:
 	"""
 	>>> get_all_patient_info("https://r4.smarthealthit.org", "bc6c8e2a-63de-4790-94af-fcab57874c21")
