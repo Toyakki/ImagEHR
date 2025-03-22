476e400fa45e262ffd9b9b64ad545b4c978fc466
Evan Su <48808396+HACKERALERT@users.noreply.github.com>
2025-03-21 23:49:34 -0400

    fhir: idea: get all patient info at once as json

--- fhir/__init__.py
+++ fhir/__init__.py
@@ -1,9 +1,10 @@
 from fhirclient.client import FHIRClient
 from fhirclient.models.patient import Patient
+from fhirclient.models.adverseevent import AdverseEvent
 
 def get_client(api_base: str) -> FHIRClient:
 	"""
-	>>> get_client("https://hapi.fhir.org/baseR4")
+	>>> get_client("https://r4.smarthealthit.org")
 	"""
 	settings = {
 		"app_id": "imagehr_bg_worker",
@@ -16,3 +17,11 @@ def get_all_patient_ids(cli: FHIRClient) -> set[str]:
 	patients = search.perform_resources(cli.server)
 	patient_ids = [patient.id for patient in patients]
 	return set(patient_ids)
+
+def get_all_patient_info_as_json(cli: FHIRClient, patient_id: str) -> str:
+	info: str = ""
+
+	# AdverseEvent
+	search = AdverseEvent.where(struct={"subject": f"Patient/{patient_id}"})
+	for event in search.perform_resources(cli.server):
+		info += event.as_json()
