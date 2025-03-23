b7805f2a10ed70a32877ac8806d0b6e04d19cfba
Evan Su <48808396+HACKERALERT@users.noreply.github.com>
2025-03-23 02:20:58 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Install PyTorch
+pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
+
 # Install dependencies
 pip install -r requirements.txt
 
